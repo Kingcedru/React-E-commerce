cd9652da3acc8954a03634e5152f96690d3fe3bb
Kingcedru <byishimocedrick@gmail.com>
2024-01-09 23:11:42 +0200

    set up and navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
+import {BsFillCartFill} from 'react-icons/bs'
+
+export default function Navbar() {
+  return (
+    <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
+      {/* left side */}
+      <div className="flex items-center">
+        <div className="cursor-pointer">
+          <AiOutlineMenu size={30} />
+        </div>
+        <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2 font-bold">
+          KETS
+        </h1>
+        <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
+          <p className="bg-black text-white rounded-full p-2">Delivery</p>
+          <p className="p-2">Pickup</p>
+        </div>
+      </div>
+      {/* Search Input I */}
+      <div className="bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px] ">
+        <AiOutlineSearch size={25} />
+        <input
+          className="bg-transparent p-2 w-full focus:outline-none"
+          type="text"
+          placeholder="Search foods"
+        />
+      </div>
+      {/* Cart button */}
+      <button className="bg-black text-white hidden md:flex items-center py-2 rounded-full">
+        <BsFillCartFill size={20} className="mr-2" />Cart
+      </button>
+    </div>
+  );
+}
