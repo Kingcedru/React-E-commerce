e1ccaf4e28c6a710a1e299755acdfbf25818983d
Kingcedru <byishimocedrick@gmail.com>
2024-01-17 19:55:57 +0200

    Headlines

--- src/components/HeadlineCards.jsx
+++ src/components/HeadlineCards.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function HeadlineCards() {
   return (
     <div className="max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6">
-      {/* Card */}
+      {/* Card 1*/}
       <div className="rounded-xl relative">
         {/* Overlay */}
         <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
@@ -14,7 +14,42 @@ export default function HeadlineCards() {
           </button>
         </div>
         <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
           src="https://images.pexels.com/photos/8471703/pexels-photo-8471703.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+        />
+      </div>
+      {/* Card 2*/}
+      <div className="rounded-xl relative">
+        {/* Overlay */}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">New Restaurants</p>
+          <p className="px-2">Added Daily</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            Order Now
+          </button>
+        </div>
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.pexels.com/photos/1860207/pexels-photo-1860207.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+        />
+      </div>
+      {/* Card 3*/}
+      <div className="rounded-xl relative">
+        {/* Overlay */}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">
+            We Deliver Desserts Too
+          </p>
+          <p className="px-2">Tasty Treats</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            Order Now
+          </button>
+        </div>
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.pexels.com/photos/2814828/pexels-photo-2814828.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
           alt=""
         />
       </div>
