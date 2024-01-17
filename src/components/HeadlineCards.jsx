f5448b62222a3308e8a4bc4796b93c499397a53e
Kingcedru <byishimocedrick@gmail.com>
2024-01-17 19:57:08 +0200

    Headlines

--- src/components/HeadlineCards.jsx
+++ src/components/HeadlineCards.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function HeadlineCards() {
   return (
     <div className="max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6">
@@ -7,7 +5,7 @@ export default function HeadlineCards() {
       <div className="rounded-xl relative">
         {/* Overlay */}
         <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
-          <p className="font-bold text-2xl px-2 pt-4">Sun's Out, BOGO's Out</p>
+          <p className="font-bold text-2xl px-2 pt-4">Suns Out, BOGOs Out</p>
           <p className="px-2">Through 8/26</p>
           <button className="border-white bg-white text-black mx-2 absolute bottom-4">
             Order Now
