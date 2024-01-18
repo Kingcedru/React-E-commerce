44e49889190df9d9931fece9d218ff3dab5d41b3
Kingcedru <byishimocedrick@gmail.com>
2024-01-18 16:36:16 +0200

    foods  done

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import { data } from "../data/data";
 
 export default function Food() {
-  console.log(data);
+  const [foods, setFoods] = useState(data);
   return (
     <div className="max-w-[1640px] m-auto px-4 py-12">
       <h1 className="text-orange-600 font-bold text-4xl text-center">
@@ -48,6 +49,28 @@ export default function Food() {
             </button>
           </div>
         </div>
+      </div>
+      {/* Display foods */}
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4">
+        {foods.map((item, index) => (
+          <div
+            key={index}
+            className="border shadow-lg rounded-lg hover:scale-105 duration-300">
+            <img
+              src={item.image}
+              alt={item.name}
+              className="w-full h-[200px] object-cover rounded-t-lg"
+            />
+            <div className="flex justify-between px-2 py-4">
+              <p className="font-bold">{item.name}</p>
+              <p>
+                <span className="bg-orange-500 text-white p-1 rounded-full">
+                  {item.price}
+                </span>
+              </p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
