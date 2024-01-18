d2d2ff63ce3a847d44ac082d1a78ea8bcc56a138
Kingcedru <byishimocedrick@gmail.com>
2024-01-18 18:13:13 +0200

    done

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { categories, data } from "../data/data";
+import { data } from "../data/data";
 
 export default function Food() {
   const [foods, setFoods] = useState(data);
